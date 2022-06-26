19a58fcb2ead3d1f5a837969e9d4053a2005e7a3
whi78 <whi951@ajou.ac.kr>
2022-06-26 14:46:04 +0900

    feat : httpsServletRequset를 이용한 값 전달
    httpsServletRequset.getparameter를 이용한 값 전달

--- src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
@@ -3,7 +3,6 @@ package hello.springmvc.basic.requestMapping;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
